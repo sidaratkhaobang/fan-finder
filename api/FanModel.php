6f9ec504e1f4b25a4fb8454db70d3f4c33919040
unknown <sunsun.np@gmail.com>
2018-09-15 14:55:47 +0700

    getNationality

--- api/FanModel.php
+++ api/FanModel.php
@@ -16,11 +16,11 @@ class FanModel
         $name_length = mb_strlen($name);
         $Nationality='';
         switch($name_length){
-            case ($name_length>=21):return'ชาติหน้า'; 
-            case ($name_length>=16):return'จีน'; 
-            case ($name_length>=11):return'วากานด้า'; 
-            case ($name_length>=6):return'ไทย'; 
-            default:return'เกาหลี'; 
+            case ($name_length>=21):$Nationality='ชาติหน้า';break;
+            case ($name_length>=16):$Nationality='จีน';break;
+            case ($name_length>=11):$Nationality='วากานด้า';break;
+            case ($name_length>=6):$Nationality='ไทย';break;
+            default:$Nationality='เกาหลี'; 
         }
         return $Nationality;
     }
