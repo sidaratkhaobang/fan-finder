cc9c4d1e6d3ec6a243195894d39b3563174490b5
DESKTOP-U4NNI57\Geforce <sidaratsamee@gmail.com>
2018-09-15 14:59:09 +0700

    team4and7

--- api/FanModel.php
+++ api/FanModel.php
@@ -14,6 +14,30 @@ class FanModel
     public static function getNationality($name)
     {
         // TODO
+        // 1-5: เกาหลี
+        // 6-10: ไทย
+        //11-15: วากานด้า
+        //16-20: จีน
+        //21++: ชาติหน้า
+        $name_length = mb_strlen($name);
+        $nationality ='';
+        if ($name_length<=5){
+            $nationality ='เกาหลี'; 
+        }
+        else if ($name_length<=10){
+            $nationality ='ไทย'; 
+        }
+        else if ($name_length<=15){
+            $nationality ='วากานด้า'; 
+        }
+        else if ($name_length<=20){
+            $nationality ='จีน'; 
+        }
+        else{
+            $nationality ='ชาติหน้า'; 
+        }
+        return $nationality; 
+
     }
 
     public static function getPlace($dob)
