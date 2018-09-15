db0256cb2138a93a44424e16730c365dff457abf
puth1998 <Teenaputh12@hotmail.com>
2018-09-15 16:52:04 +0700

    done config

--- api/FanModel.php
+++ api/FanModel.php
@@ -11,30 +11,30 @@ class FanModel
         ];
     }
 
-    // public static function getNationality($name)
-    // {
-    //     // TODO
-    //     // 1-5: เกาหลี
-    //     // 6-10: ไทย
-    //     // 11-15: วากานด้า
-    //     // 16-20: จีน
-    //     // 21++ : ชาติหน้า
+    public static function getNationality($name)
+    {
+        // TODO
+        // 1-5: เกาหลี
+        // 6-10: ไทย
+        // 11-15: วากานด้า
+        // 16-20: จีน
+        // 21++ : ชาติหน้า
         
-    //     $name_length = mb_strlen($name);
-    //     $nationality = '';
-    //     if($name_length <= 5  ){
-    //         $nationality = 'เกาหลี';
-    //     }else if($name_length <= 10 ){
-    //         $nationality = 'ไทย';
-    //     }else if($name_length <= 15 ){
-    //         $nationality = 'วากานด้า';
-    //     }else if($name_length <= 20 ){
-    //         $nationality = 'จีน';
-    //     }else{
-    //         $nationality = 'ชาติหน้า';
-    //     }  
-    //     return $nationality;
-    // }
+        $name_length = mb_strlen($name);
+        $nationality = '';
+        if($name_length <= 5  ){
+            $nationality = 'เกาหลี';
+        }else if($name_length <= 10 ){
+            $nationality = 'ไทย';
+        }else if($name_length <= 15 ){
+            $nationality = 'วากานด้า';
+        }else if($name_length <= 20 ){
+            $nationality = 'จีน';
+        }else{
+            $nationality = 'ชาติหน้า';
+        }  
+        return $nationality;
+    }
 
     public static function getPlace($dob)
     {
@@ -49,26 +49,25 @@ class FanModel
         $score = array_sum($spDob)%10;
         if($score == 9  ){
             $place = 'บางประกง';
-        }
-        // }else if($dob == 8 ){
-        //     $place = 'โนเแวร์';
-        // }else if($dob == 7 ){
-        //     $place = 'โอคแลนด์';
-        // }else if($dob == 6  ){
-        //     $place = 'โซล';
-        // }else if($dob == 5 ){
-        //     $place = 'บรูคลิน';
-        // }else if($dob == 4 ){
-        //     $place = 'แอสการ์ด';
-        // }else if($dob == 3  ){
-        //     $place = 'แซนดร้า';
-        // }else if($dob == 2 ){
-        //     $place = 'โซโคเวีย';
-        // }else if($dob == 1 ){
-        //     $place = 'รัฐเวอร์เรีย';
-        // }else{
-        //     $place = 'วากานด้า';
-        // }  
+        }else if($score == 8 ){
+            $place = 'โนเแวร์';
+        }else if($score == 7 ){
+            $place = 'โอคแลนด์';
+        }else if($score == 6  ){
+            $place = 'โซล';
+        }else if($score == 5 ){
+            $place = 'บรูคลิน';
+        }else if($score == 4 ){
+            $place = 'แอสการ์ด';
+        }else if($score == 3  ){
+            $place = 'แซนดร้า';
+        }else if($score == 2 ){
+            $place = 'โซโคเวีย';
+        }else if($score == 1 ){
+            $place = 'รัฐเวอร์เรีย';
+        }else{
+            $place = 'วากานด้า';
+        }  
         return $place;
     }
     
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -14,91 +14,91 @@ final class FanModelTest extends TestCase
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
     }
-    // public function testGetNationality_NameLength4(){
-    //     $expected_result = 'เกาหลี';
-    //     $result = FanModel::getNationality('หมวย');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetNationality_NameLength4(){
+        $expected_result = 'เกาหลี';
+        $result = FanModel::getNationality('หมวย');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetNationality_NameLength7(){
-    //     $expected_result = 'ไทย';
-    //     $result = FanModel::getNationality('ดอกหมวย');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetNationality_NameLength7(){
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('ดอกหมวย');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetNationality_NameLength11(){
-    //     $expected_result = 'วากานด้า';
-    //     $result = FanModel::getNationality('ดอกหมวยอิอิ');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetNationality_NameLength11(){
+        $expected_result = 'วากานด้า';
+        $result = FanModel::getNationality('ดอกหมวยอิอิ');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetNationality_NameLength16(){
-    //     $expected_result = 'จีน';
-    //     $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ย');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetNationality_NameLength16(){
+        $expected_result = 'จีน';
+        $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ย');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetNationality_NameLength21(){
-    //     $expected_result = 'ชาติหน้า';
-    //     $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ยมากๆๆ');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetNationality_NameLength21(){
+        $expected_result = 'ชาติหน้า';
+        $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ยมากๆๆ');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob0(){
-    //     $expected_result = 'วากานด้า';
-    //     $result = FanModel::getPlace('0');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob0(){
+        $expected_result = 'วากานด้า';
+        $result = FanModel::getPlace('01/06/2001');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob1(){
-    //     $expected_result = 'รัฐเวอร์เรีย';
-    //     $result = FanModel::getPlace('1');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob1(){
+        $expected_result = 'รัฐเวอร์เรีย';
+        $result = FanModel::getPlace('02/06/2001');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob2(){
-    //     $expected_result = 'โซโคเวีย';
-    //     $result = FanModel::getPlace('2');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob2(){
+        $expected_result = 'โซโคเวีย';
+        $result = FanModel::getPlace('03/06/2001');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob3(){
-    //     $expected_result = 'แซนดร้า';
-    //     $result = FanModel::getPlace('3');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob3(){
+        $expected_result = 'แซนดร้า';
+        $result = FanModel::getPlace('04/06/2001');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob4(){
-    //     $expected_result = 'แอสการ์ด';
-    //     $result = FanModel::getPlace('4');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob4(){
+        $expected_result = 'แอสการ์ด';
+        $result = FanModel::getPlace('05/06/2001');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob5(){
-    //     $expected_result = 'บรูคลิน';
-    //     $result = FanModel::getPlace('5');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob5(){
+        $expected_result = 'บรูคลิน';
+        $result = FanModel::getPlace('10/03/2540');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob6(){
-    //     $expected_result = 'โซล';
-    //     $result = FanModel::getPlace('6');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob6(){
+        $expected_result = 'โซล';
+        $result = FanModel::getPlace('10/04/2540');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob7(){
-    //     $expected_result = 'โอคแลนด์';
-    //     $result = FanModel::getPlace('7');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob7(){
+        $expected_result = 'โอคแลนด์';
+        $result = FanModel::getPlace('10/05/2540');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    // public function testGetPlace_Dob8(){
-    //     $expected_result = 'โนเแวร์';
-    //     $result = FanModel::getPlace('8');
-    //     $this->assertEquals($expected_result, $result);
-    // }
+    public function testGetPlace_Dob8(){
+        $expected_result = 'โนเแวร์';
+        $result = FanModel::getPlace('11/05/2540');
+        $this->assertEquals($expected_result, $result);
+    }
 
-    public function testGetPlace_Dob(){
+    public function testGetPlace_Dob9(){
         $expected_result = 'บางประกง';
         $result = FanModel::getPlace('21/05/2540');
         $this->assertEquals($expected_result, $result);
