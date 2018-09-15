54be99d6f761c468666070b30b830e226084d428
firsticz <thanawat6368@gmail.com>
2018-09-15 14:48:46 +0700

    add test korea and thai

--- api/FanModel.php
+++ api/FanModel.php
@@ -5,16 +5,31 @@ class FanModel
     public static function predict($name, $dob, $mobileno)
     {
         return [
-            "nationality" => 'อังกฤษ',
-            "place" => 'ยอดดอยอันหนาวเหน็บ',
-            'action' => 'ปิ้งไก่',
+            "nationality" => 'ฝรั่งเศษ',
+            "place" => 'ทุ่งหญ้าลาเวนเดอร์',
+            'action' => 'ปิ้งกบ',
         ];
     }
 
     public static function getNationality($name)
     {
         // TODO
+        //1-5: เกาหลี
+        //6-10: ไทย
+        //11-15: วากานด้า
+        //16-20: จีน
+        //21++: คองโก
+        $name_length = mb_strlen($name);
+        $nationality = '';
+        if ($name_length <=5){
+        $nationality = 'เกาหลี';
+        }
+        elseif($name_length <=10){
+            $nationality = 'ไทย';
+        }
+        return $nationality;
     }
+
 
     public static function getPlace($dob)
     {
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -7,11 +7,25 @@ final class FanModelTest extends TestCase
     public function testPredict(): void
     {
         $expected_result = [
-            "nationality" => 'อังกฤษ',
-            "place" => 'ยอดดอยอันหนาวเหน็บ',
-            'action' => 'ปิ้งไก่' 
+            "nationality" => 'ฝรั่งเศษ',
+            "place" => 'ทุ่งหญ้าลาเวนเดอร์',
+            'action' => 'ปิ้งกบ' 
         ];
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
+    }
+    public function testGetNationality_NameLength4()
+    {
+        $expected_result = 'เกาหลี';
+        $result = FanModel::getNationality('หมวย');
+        $this->assertEquals($expected_result,$result);
+
+    }
+    public function testGetNationality_NameLength7()
+    {
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('หมวยหมก');
+        $this->assertEquals($expected_result,$result);
+
     }
 }
