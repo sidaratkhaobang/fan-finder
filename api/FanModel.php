9d16e2817e96a74515930cbc88b5f7711b5813ff
unknown <bestnacufzxzx1@gmail.com>
2018-09-15 15:20:13 +0700

    test case01

--- api/FanModel.php
+++ api/FanModel.php
@@ -14,6 +14,29 @@ class FanModel
     public static function getNationality($name)
     {
         // TODO
+        // 1-5: เกาหลี
+        // 6-10: ไทย
+        // 11-15: วากานด้า
+        // 16-20: จีน
+        // 21++: ชาตหน้า
+        $name_length = mb_strlen($name);
+        $nationaliy = '';
+        if ($name_length <= 5) {
+            $nationaliy = 'เกาหลี';
+        }
+        else if($name_length<=10){
+            $nationaliy = 'ไทย';
+        }
+        else if($name_length<=15){
+            $nationaliy = 'วากานด้า';
+        }
+        else if($name_length<=20){
+            $nationaliy = 'จีน';
+        }
+        else{
+            $nationaliy = 'ชาตหน้า';
+        }
+        return $nationaliy;
     }
 
     public static function getPlace($dob)
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -14,4 +14,30 @@ final class FanModelTest extends TestCase
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
     }
+    public function testGetNationaliy_NameLength4()
+    {
+        $expected_result = 'เกาหลี';
+        $result = FanModel::getNationality('หมวย');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGetNationaliy_NameLength7()
+    {
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('หมวยลอย');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGetNationaliy_NameLength10()
+    {
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('หมวยลอยได้');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGetNationaliy_NameLength15()
+    {
+        $expected_result = 'วากานด้า';
+        $result = FanModel::getNationality('หมวยลอยได้กรุบๆ');
+        $this->assertEquals($expected_result, $result);
+    }
+
+   
 }
