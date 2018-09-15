fe8914bcda8708ea4814c78542b287dcf260e1e0
unknown <gaidmanee@gmail.com>
2018-09-15 14:55:38 +0700

    test nationallity

--- api/FanModel.php
+++ api/FanModel.php
@@ -14,6 +14,30 @@ class FanModel
     public static function getNationality($name)
     {
         // TODO
+        // 1-5:เกาหลี
+        // 6-10: ไทย
+        // 11-15: วากานด้า
+        // 16-20:จีน
+        // 21++:คองโก 
+        $name_length = mb_strlen($name);
+        $nationality = '';
+        if ($name_length <= 5){
+            $nationality = 'เกาหลี';
+        }
+        elseif($name_length<=10){
+            $nationality = 'ไทย';
+        }
+        elseif($name_length<=15){
+            $nationality = 'วากานด้า';
+        }
+        
+        elseif($name_length<=20){
+            $nationality = 'จีน';
+        }
+        else {
+            $nationality = 'คองโก';
+        }
+        return $nationality;
     }
 
     public static function getPlace($dob)
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -14,4 +14,34 @@ final class FanModelTest extends TestCase
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
     }
+    public function testGetNationality_NameLength4()
+    {
+        $expected_result = 'เกาหลี';
+        $result = FanModel::getNationality('หมวย');
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetNationality_NameLength7()
+    {
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('ดอกหมวย');
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetNationality_NameLength13()
+    {
+        $expected_result = 'วากานด้า';
+        $result = FanModel::getNationality('หมวยดอกดอกดอก');
+        $this->assertEquals($expected_result,$result);
+    } 
+    public function testGetNationality_NameLength16()
+    {
+        $expected_result = 'จีน';
+        $result = FanModel::getNationality('ดอกหมวยดอกดอกดอก');
+        $this->assertEquals($expected_result,$result);
+    }   
+    public function testGetNationality_NameLength21()
+    {
+        $expected_result = 'คองโก';
+        $result = FanModel::getNationality('ดอกดอดอกหมวยดอกดอกดอก');
+        $this->assertEquals($expected_result,$result);
+    }   
 }
