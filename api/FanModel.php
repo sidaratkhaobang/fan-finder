3573065a7823be502c11aa2fc28e2664fd11f98a
Yodev <natlove.natlove202@gmail.com>
2018-09-15 14:52:58 +0700

    add commit

--- api/FanModel.php
+++ api/FanModel.php
@@ -5,15 +5,42 @@ class FanModel
     public static function predict($name, $dob, $mobileno)
     {
         return [
-            "nationality" => 'อังกฤษ',
-            "place" => 'ยอดดอยอันหนาวเหน็บ',
-            'action' => 'ปิ้งไก่',
+            // "nationality" => 'อังกฤษ1',
+            // "place" => 'ยอดดอยอันหนาวเหน็บ',
+            // 'action' => 'ปิ้งไก่',
+            "nationality" => 'ไทย',
+            "place" => 'อาคารเรียนรวม 3',
+            'action' => 'ขึ้นบันไดอาคารเรียนรวม 3'
         ];
     }
 
     public static function getNationality($name)
     {
-        // TODO
+        //1-5 : เกาหลี
+        //6-10 : ไทย
+        //11-15 : วากานด้า
+        //16-20 : จีน
+        //21++ : ชาติหน้า
+
+        $name_lenght = mb_strlen($name);
+        $nationlity = " ";
+        if($name_lenght <= 5){
+            $nationlity ="เกาหลี";
+        }
+        else if($name_lenght <= 10){
+            $nationlity ="ไทย";
+        }
+        else if($name_lenght <= 15){
+            $nationlity ="วากานด้า";
+        }
+        else if($name_lenght <= 20){
+            $nationlity ="จีน";
+        }
+        else {
+            $nationlity="ชาติหน้า";
+        }
+        return $nationlity;
+        // return "เกาหลี";
     }
 
     public static function getPlace($dob)
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -6,12 +6,51 @@ final class FanModelTest extends TestCase
 {
     public function testPredict(): void
     {
+
+        // $name ="Nat";
+        // $dob =" 2 april 1997";
+        // $mobileno = "0867454630";
+        // $expected_result = [
+        //     "nationality" => 'อังกฤษ1',
+        //     "place" => 'ยอดดอยอันหนาวเหน็บ',
+        //     'action' => 'ปิ้งไก่' 
+        // ];
         $expected_result = [
-            "nationality" => 'อังกฤษ',
-            "place" => 'ยอดดอยอันหนาวเหน็บ',
-            'action' => 'ปิ้งไก่' 
+            "nationality" => 'ไทย',
+            "place" => 'อาคารเรียนรวม 3',
+            'action' => 'ขึ้นบันไดอาคารเรียนรวม 3' 
         ];
         $result = FanModel::predict($name, $dob, $mobileno);
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGatNationality_NameLenght4()
+    {
+        $expected_result ='เกาหลี';
+        $result = FanModel::getNationality('[บี]');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGatNationality_NameLenght10()
+    {
+        $expected_result ='ไทย';
+        $result = FanModel::getNationality('[หมวยดก]');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGatNationality_NameLenght15()
+    {
+        $expected_result ='วากานด้า';
+        $result = FanModel::getNationality('[ฟิวฟี่ขี้ลัก]');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGatNationality_NameLenght20()
+    {
+        $expected_result ='จีน';
+        $result = FanModel::getNationality('[ณัฐวุฒิชอบขุดดิน]');
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGatNationality_NameLenghtmore21()
+    {
+        $expected_result ='ชาติหน้า';
+        $result = FanModel::getNationality('[ทีเด็ดเฟี้ยวฟ้าวเยี่ยวราดพื้นลื่นหัวทิ่มดิน]');
         $this->assertEquals($expected_result, $result);
     }
 }
