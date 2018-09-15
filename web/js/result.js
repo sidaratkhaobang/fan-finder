354301e1e2ea3ec6ce9f55a8431acbcb2d000234
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-09-16 02:13:51 +0700

    Add play again port

--- web/js/result.js
+++ web/js/result.js
@@ -8,10 +8,17 @@ document.getElementById("nationality").innerHTML = nationality;
 document.getElementById("place").innerHTML = place;
 document.getElementById("action").innerHTML = action;
 document.getElementById("share-button").addEventListener('click', function() {
-  console.log(window.location.href);
   FB.ui({
     method: 'share',
     href: window.location.href,
     quote: `สวรรค์มีตา !!! เนื้อคู่ของคุณจะเป็นคน "${nationality}" พบกันที่ "${place}" ในขณะที่คุณกำลัง "${action}"`,
   }, function (response) { });
 });
+
+function playAgain() {
+  const url_string = window.location.href;
+  const url = new URL(url_string);
+  const port = url.searchParams.get("port");
+  const redirect_url = `http://${url.hostname}:${port}`;
+  window.location = redirect_url;
+}
--- web/js/script.js
+++ web/js/script.js
@@ -5,17 +5,19 @@ function predict(event) {
 
   const Http = new XMLHttpRequest();
   const current_url = new URL(window.location.href);
+  const web_port = current_url.port;
   const api_port = parseInt(current_url.port) + 1;
-  const host = `http://${current_url.hostname}:${api_port}`;
-  const url = `${host}/fan/predict?name=${name}&mobileno=${phone}&dob=${birthdate}`;
-  Http.open("GET", url, true);
+  const api_host = `http://${current_url.hostname}:${api_port}`;
+  const result_host = `http://${current_url.hostname}:2000`;
+  const api_url = `${api_host}/fan/predict?name=${name}&mobileno=${phone}&dob=${birthdate}`;
+  Http.open("GET", api_url, true);
   Http.send();
 
   Http.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
       const resp = JSON.parse(Http.responseText);
       const { nationality, place, action } = resp.data;
-      const result_url = `result.html?nationality=${nationality}&place=${place}&action=${action}`;
+      const result_url = `${result_host}/result.html?port=${web_port}&nationality=${nationality}&place=${place}&action=${action}`;
       window.location = result_url;
     }
   }
