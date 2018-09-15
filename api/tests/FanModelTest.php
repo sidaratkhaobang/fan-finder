f8f3c1895eba8f4fe68fc53795d800930947955c
puth1998 <Teenaputh12@hotmail.com>
2018-09-15 14:44:58 +0700

    test

--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -14,4 +14,34 @@ final class FanModelTest extends TestCase
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
     }
+    public function testGetNationality_NameLength4(){
+        $expected_result = 'เกาหลี';
+        $result = FanModel::getNationality('หมวย');
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testGetNationality_NameLength7(){
+        $expected_result = 'ไทย';
+        $result = FanModel::getNationality('ดอกหมวย');
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testGetNationality_NameLength11(){
+        $expected_result = 'วากานด้า';
+        $result = FanModel::getNationality('ดอกหมวยอิอิ');
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testGetNationality_NameLength16(){
+        $expected_result = 'จีน';
+        $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ย');
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testGetNationality_NameLength21(){
+        $expected_result = 'ชาติหน้า';
+        $result = FanModel::getNationality('ดอกหมวยอิอิเหี้ยมากๆๆ');
+        $this->assertEquals($expected_result, $result);
+    }
+    
 }
