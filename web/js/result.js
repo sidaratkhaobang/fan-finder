346335e6a0eeb398a8f0b015ded0c2c003a73357
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-09-16 01:18:21 +0700

    Add share button

--- web/js/result.js
+++ web/js/result.js
@@ -3,3 +3,10 @@ const url = new URL(url_string);
 document.getElementById("nationality").innerHTML = url.searchParams.get("nationality");
 document.getElementById("place").innerHTML = url.searchParams.get("place");
 document.getElementById("action").innerHTML = url.searchParams.get("action");
+document.getElementById("share-button").addEventListener('click', function() {
+  FB.ui({
+    method: 'share',
+    href: window.location.href,
+    quote: `สวรรค์มีตา !!! เนื้อคู่ของคุณจะเป็นคน "${nationality}" พบกันที่ "${place}" ในขณะที่คุณกำลัง "${action}"`,
+  }, function (response) { });
+});
