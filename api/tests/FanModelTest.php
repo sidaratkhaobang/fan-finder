4c3b0fe9d3f80b6979328a3e5150d2d1c836610f
DESKTOP-U4NNI57\Geforce <sidaratsamee@gmail.com>
2018-09-15 14:56:40 +0700

    Team4and7

--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -14,4 +14,35 @@ final class FanModelTest extends TestCase
         $result = FanModel::predict($name, $dob, $mobileno);
         $this->assertEquals($expected_result, $result);
     }
-}
+    public function testGetNationality_Namelength4()
+        {
+            $expected_result = 'เกาหลี';
+            $result = FanModel::getNationality('หมวย');
+            $this->assertEquals($expected_result, $result); 
+        }
+    public function testGetNationality_Namelength7()
+        {
+            $expected_result = 'ไทย';
+            $result = FanModel ::getNationality('อะวาฏิฟ');
+            $this->assertEquals($expected_result, $result); 
+        }
+        public function testGetNationality_Namelength11()
+        {
+            $expected_result = 'วากานด้า';
+            $result = FanModel ::getNationality('กขคงจนยาเด็กญ');
+            $this->assertEquals($expected_result, $result); 
+        }
+        public function testGetNationality_Namelength16()
+        {
+            $expected_result = 'จีน';
+            $result = FanModel ::getNationality('อิทซาลินซิลเวียร');
+            $this->assertEquals($expected_result, $result); 
+        }
+        public function testGetNationality_Namelength21()
+        {
+            $expected_result = 'ชาติหน้า';
+            $result = FanModel ::getNationality('สิดารัศมิ์สะแมวววววก๊ะและบริวาร');
+            $this->assertEquals($expected_result, $result); 
+        }
+} 
+
