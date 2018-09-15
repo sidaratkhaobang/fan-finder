65502ca9f68d325b707fd07b8a840231f12e1218
DESKTOP-U4NNI57\Geforce <sidaratsamee@gmail.com>
2018-09-15 17:13:50 +0700

    team4

--- api/FanModel.php
+++ api/FanModel.php
@@ -30,7 +30,7 @@ class FanModel
         else if ($name_length<=15){
             $nationality ='วากานด้า'; 
         }
-        else if ($name_length<=20){
+        else if ($name_length<=21){
             $nationality ='จีน'; 
         }
         else{
@@ -43,8 +43,76 @@ class FanModel
     public static function getPlace($dob)
     {
         // TODO
-    }
+        // 11/05/2540 (DD/MM/YYYY)
+        // score = (1+1+0+5+2+5+4+0)%10
+        // 0:
+        // 1:
+        // 2:
+        // 3:
+        // 4:
+        // 5:
+        // 6:
+        // 7:
+        // 8:
+        // 9:
+        //HINT: str_replace, str_split
+        $sum=0;
+        $day_birthday = str_replace('/','',$dob);
+        $num_birthday = str_split($day_birthday);
+        $place = '';
+        foreach($num_birthday as $value){
+       
+            $sum += intval($value);
+        }
+        switch ($sum%10){
+            case 0: $place = 'Paris'; break;
+            case 1: $place = 'Soul' ; break;
+            case 2: $place = 'bangkok'; break;
+            case 3: $place = 'London'; break;
+            case 4: $place = 'japan'; break;
+            case 5: $place = 'USA' ; break;
+            case 6: $place = 'Sweden';  break;
+            case 7: $place = 'Arizona' ;break;
+            case 8: $place = 'Spain'; break;
+            case 9: $place = 'peru'; break;
 
+            
+        }
+        return $place;
+
+            // if ($day_birthday=0){
+            //     $place ='Paris'; 
+            // }
+            // else if ($day_birthday=1){
+            //     $place ='Soul'; 
+            // }
+            // else if ($day_birthday=2){
+            //     $place ='bangkok'; 
+            // }
+            // else if ($day_birthday=3){
+            //     $place ='London'; 
+            // }
+            // else if ($day_birthday=4){
+            //     $place ='japan'; 
+            // }
+            // else if ($day_birthday=5){
+            //     $place ='USA'; 
+            // }
+            // else if ($day_birthday=6){
+            //     $place ='Sweden'; 
+            // }
+            // else if ($day_birthday=7){
+            //     $place ='Arizona'; 
+            // }
+            // else if ($day_birthday=8){
+            //     $place ='Spain'; 
+            // }
+            // else {
+            //     $place ='peru'; 
+            // }
+        }
+
+    
     public static function getAction($mobileno)
     {
         // TODO
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -29,7 +29,7 @@ final class FanModelTest extends TestCase
         public function testGetNationality_Namelength11()
         {
             $expected_result = 'วากานด้า';
-            $result = FanModel ::getNationality('กขคงจนยาเด็กญ');
+            $result = FanModel ::getNationality('วิชุตาหมาดอะดำ');
             $this->assertEquals($expected_result, $result); 
         }
         public function testGetNationality_Namelength16()
@@ -43,6 +43,68 @@ final class FanModelTest extends TestCase
             $expected_result = 'ชาติหน้า';
             $result = FanModel ::getNationality('สิดารัศมิ์สะแมวววววก๊ะและบริวาร');
             $this->assertEquals($expected_result, $result); 
+        }
+
+
+        public function testDayOfBirthday0()
+        {
+            $expected_result = 'Paris';
+            $result = FanModel ::getPlace('01/02/2005');//10%10=0
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday1()
+        {
+            $expected_result = 'Soul';
+            $result = FanModel ::getPlace('04/06/2540');//21%10=1
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday2()
+        {
+            $expected_result = 'bangkok';
+            $result = FanModel ::getPlace('04/06/2541');//22%10=2
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday3()
+        {
+            $expected_result = 'London';
+            $result = FanModel ::getPlace('06/07/2503');//23%10=3
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday4()
+        {
+            $expected_result = 'japan';
+            $result = FanModel ::getPlace('08/06/2503');//24%10=4
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday5()
+        {
+            $expected_result = 'USA';
+            $result = FanModel ::getPlace('06/07/2541');//25%10=5
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday6()
+        {
+            $expected_result = 'Sweden';
+            $result = FanModel ::getPlace('08/06/2541');//26%10=6
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday7()
+        {
+            $expected_result = 'Arizona';
+            $result = FanModel ::getPlace('15/06/2544');//27%10=7
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday8()
+        {
+            $expected_result = 'Spain';
+            $result = FanModel ::getPlace('19/06/2541');//28%10=8
+            $this->assertEquals($expected_result,$result);
+        }
+        public function testDayOfBirthday9()
+        {
+            $expected_result = 'peru';
+            $result = FanModel ::getPlace('06/07/2536');//29%10=9
+            $this->assertEquals($expected_result,$result);
         }
 } 
 
