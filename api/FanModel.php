e018396e389f808f3d976058987d1826693d0bec
SkyChonlatan <guy_ck@hotmail.com>
2018-09-16 10:12:34 +0700

    testGetAction

--- api/FanModel.php
+++ api/FanModel.php
@@ -45,12 +45,23 @@ class FanModel
         // TODO
         // 11/05/2540 (DD//MM/YYYY)
         // sum = (1+1+0+5+2+5+4+0)%10
-        // HINT : str_replace,str_split
-        // 1
-        // 2
-        // 3
-        // 4
-        // 5
+        $number_without_slash = str_replace('/','',$dob);
+        $number_array = str_split($number_without_slash);
+        $score = array_sum($number_array)%10;
+        $places = [
+            'ถ้ำหลวง',
+            'ดาวอังคาร',
+            'ในป่า',
+            'ทำเนียบรัฐบาล',
+            'เรือนจำกลาง',
+            'เอเวอร์เรส',
+            'ใต้มหาสมุทรแปซิฟิก',
+            'วลัยลักษณ์',
+            '3เวย์',
+            'ลานชา',
+        ];
+        return $places[$score];
+        
         
 
     }
@@ -58,5 +69,22 @@ class FanModel
     public static function getAction($mobileno)
     {
         // TODO
+        $number_without_slash = str_replace('-','',$mobileno);
+        $number_array = str_split($number_without_slash);
+        $score = array_sum($number_array)%10;
+        $places = [
+             'ยิงตัวตาย',
+            'ทอดไก่ด้วยน้ำมันหมู',
+            'ทอดหมูด้วยน้ำมันรถ',
+            'ยำเนื้อ',
+            'ตรวจโรคเอดส์',
+            'ฉลามเเดกก',
+            'กระทืบยามหน้าหอ',
+            'หนีตำรวจ',
+            'คุยกับคนบ้า',
+            'ไถ่ตังคนเเก่เเตะคนท้อง',
+        ];
+        return $places[$score];
+
     }
 }
--- api/tests/FanModelTest.php
+++ api/tests/FanModelTest.php
@@ -41,9 +41,104 @@ final class FanModelTest extends TestCase
         $result = FanModel :: getNationality('วากานด้าไม่ใช่สถานที่แต่เป็นผู้คน');
         $this->assertEquals($expected_result,$result);
     }
-    public function testGet1(){
+    public function testGetPlace_Num0(){
+        $expected_result = 'ถ้ำหลวง';
+        $result = FanModel :: getPlace('22/02/2002'); //10
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num1(){
         $expected_result = 'ดาวอังคาร';
-        $result = FanModel :: getPlace('20/01/1501');
+        $result = FanModel :: getPlace('20/01/1601'); //11
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num2(){
+        $expected_result = 'ในป่า';
+        $result = FanModel :: getPlace('31/01/2005');//12
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num3(){
+        $expected_result = 'ทำเนียบรัฐบาล';
+        $result = FanModel :: getPlace('01/02/1900');//13
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num4(){
+        $expected_result = 'เรือนจำกลาง';
+        $result = FanModel :: getPlace('18/01/2002'); //14
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num5(){
+        $expected_result = 'เอเวอร์เรส';
+        $result = FanModel :: getPlace('18/02/2002');//15
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num6(){
+        $expected_result = 'ใต้มหาสมุทรแปซิฟิก';
+        $result = FanModel :: getPlace('27/03/2002');//16
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num7(){
+        $expected_result = 'วลัยลักษณ์';
+        $result = FanModel :: getPlace('18/04/2002');//17
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num8(){
+        $expected_result = '3เวย์';
+        $result = FanModel :: getPlace('19/04/2002');//18
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetPlace_Num9(){
+        $expected_result = 'ลานชา';
+        $result = FanModel :: getPlace('28/05/2002');//19
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction0(){
+        $expected_result = 'ยิงตัวตาย';
+        $result = FanModel :: getAction('088-0000-04');//20
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction1(){
+        $expected_result = 'ทอดไก่ด้วยน้ำมันหมู';
+        $result = FanModel :: getAction('089-0000-04');//21
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction2(){
+        $expected_result = 'ทอดหมูด้วยน้ำมันรถ';
+        $result = FanModel :: getAction('089-1000-04');//22
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction3(){
+        $expected_result = 'ยำเนื้อ';
+        $result = FanModel :: getAction('089-1010-04');//23
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction4(){
+        $expected_result = 'ตรวจโรคเอดส์';
+        $result = FanModel :: getAction('089-1020-04');//24
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction5(){
+        $expected_result = 'ฉลามเเดกก';
+        $result = FanModel :: getAction('089-1020-14');//25
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction6(){
+        $expected_result = 'กระทืบยามหน้าหอ';
+        $result = FanModel :: getAction('099-0102-14');//26
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction7(){
+        $expected_result = 'หนีตำรวจ';
+        $result = FanModel :: getAction('099-0103-14');//27
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction8(){
+        $expected_result = 'คุยกับคนบ้า';
+        $result = FanModel :: getAction('099-0103-15');//28
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testGetAction9(){
+        $expected_result = 'ไถ่ตังคนเเก่เเตะคนท้อง';
+        $result = FanModel :: getAction('099-0105-14');//29
         $this->assertEquals($expected_result,$result);
     }
 }
